9d219b7e9e293d62fbcc146ff93bed5190c1c6ea
Jader Dias <52470+JaderDias@users.noreply.github.com>
2022-04-21 09:25:49 +0000

    print random words

--- python/croatianbot/Content.py
+++ python/croatianbot/Content.py
@@ -1,15 +1,13 @@
-import Storage
 import re
+import random
 
 title_matcher = re.compile("^([^=]+)(=.*)$")
 escaped_new_line = re.compile("\\\\n")
-h3_search = re.compile(r"===(Adjective|Noun|Verb)=[^#]+# ([^=]+)", re.DOTALL)
-hashtag_search = re.compile("#")
+h3_search = re.compile(r"===(Adjective|Adverb|Interjection|Phrase|Pronoun|Noun|Verb)=[^#]+# ([^=]+)", re.DOTALL)
 curly_link_search = re.compile(r"\{\{[^}]*[|=]([^|}=]+)\}\}")
 square_link_search_1 = re.compile(r"\[\[[^|]*\|([^|\]]*)\]\]")
 square_link_search_2 = re.compile(r"\[\[([^|\]]*)\]\]")
-reserved_characters_search = re.compile(r"([-.|!()=#*{}\[\]])")
-split_definitions = re.compile(r"\\\*")
+split_definitions = re.compile(r"#")
 
 def _parse(line: str) -> tuple:
     title_match = title_matcher.match(line).groups()
@@ -20,27 +18,24 @@ def _parse(line: str) -> tuple:
     (grammatical_class, definition) = h3_search.search(definition).groups()
     print(grammatical_class)
     print(definition)
-    definition = curly_link_search.sub(r"(_\1_)", definition)
+    definition = curly_link_search.sub(r"(\1)", definition)
     definition = square_link_search_1.sub(r"\1", definition)    
     definition = square_link_search_2.sub(r"\1", definition)
-    definition = hashtag_search.sub("*", definition)
-    definition = reserved_characters_search.sub(r"\\\1", definition)
     definitions = split_definitions.split(definition)
     return (title, definitions, grammatical_class)
 
 def get() -> str:
-    pub_count = Storage.read_pub_count()
+    target_line = random.randint(0, 19000)
     with open("sh.csv", "r") as f:
         line_count = 0
         line = f.readline()
         while line:
-            if line_count > pub_count:
+            if line_count > target_line:
                 (title, definitions, grammatical_class) = _parse(line)
                 if title and len(definitions) > 0 and grammatical_class:
-                    result = f"Croatian {grammatical_class} of the hour:\n\"{title}\"\nDefinition:"
+                    result = f"Croatian {grammatical_class} of the hour:\n\"{title}\"\nDefinition:\n"
                     for i in range(len(definitions)):
-                        result += f"\n{i+1}. {definitions[i]}"
-                    Storage.update_pub_count(line_count)
+                        result += f"{i+1}. {definitions[i]}"
                     return result
             line_count += 1
             line = f.readline()
